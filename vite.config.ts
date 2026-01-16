1747d5b9e1e61cf4dcee78f04000f77289014bf4
fullshop <irsam8552@gmail.com>
2026-01-16 02:53:10 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/your-repository-name/', // Replace with your actual repo name
+  base: '/shopy-z/', // Replace with your actual repo name
   plugins: [react()],
 })
